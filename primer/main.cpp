ba7f8b9593911d53b07a3a539f26a482fff9c055
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-16 21:36:35 +0200

    Exersise 1.5: We wrote the output in one large statement. Rewrite the program to use a separate statement to print each operand.

--- primer/main.cpp
+++ primer/main.cpp
@@ -3,16 +3,21 @@
 int main()
 {
 	// ---------------------------------------------------------------------------
-	// Exersise 1.4: 
-	// Our program used the addition operator, +, to add two numbers. Write a 
-	// program that uses the multiplication operator, *, to print the product 
-	// instead
+	// Exersise 1.5: 
+	// We wrote the output in one large statement. Rewrite the program to use a 
+	// separate statement to print each operand.
 	// ---------------------------------------------------------------------------
 
 	std::cout << "Enter two numbers:" << std::endl;
 	int v1 = 0, v2 = 0;
 	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+	std::cout << "The sum of ";
+	std::cout << v1;
+	std::cout << " and ";
+	std::cout << v2;
+	std::cout << " is ";
+	std::cout << v1 * v2;
+	std::cout << std::endl;
 
 	return 0;
 }
