d6ff6bdf5043d2292b376c1d8fd6a97e20e27cfc
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-16 22:07:49 +0200

    single-line comment content is ignored.

--- primer/main.cpp
+++ primer/main.cpp
@@ -1,9 +1,4 @@
-/*
- * comment pairs /* */ cannot nest.
- * "cannot nest" is considered source code,
- * as is the rest of the program
- */
-int main()
-{
-	return 0;
-}
+// /*
+//  * Everything inside a single-line comment is ignored
+//  * including nested comment pairs
+//  */
