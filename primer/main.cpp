0c5204a6cda7c4bd355be100527cdda48a143cb8
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-16 22:11:14 +0200

    Excerside 1.7: Compile a program that has incorrectly nested comments.

--- primer/main.cpp
+++ primer/main.cpp
@@ -1,4 +1,14 @@
-// /*
-//  * Everything inside a single-line comment is ignored
-//  * including nested comment pairs
-//  */
+#include <iostream>
+
+int main()
+{
+	// ---------------------------------------------------------------------------
+	// Excersise 1.7:
+	// Compile a program that has incorrectly nested comments.
+	// ---------------------------------------------------------------------------
+
+	/*
+	 * lets test this. */ /* another comment // single-line comment nested inside
+	 multi-line comment */
+	 */
+}
