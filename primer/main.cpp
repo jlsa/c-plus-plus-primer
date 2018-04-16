7aa57c4a7db18c4e9bdc920228c183c41dca957f
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-16 21:34:32 +0200

    Exersise 1.4: Our program used the addition operator, +, to add two numbers. Write a program that uses the multiplication operator, *, to print the product instead.

--- primer/main.cpp
+++ primer/main.cpp
@@ -2,10 +2,17 @@
 
 int main()
 {
-	// --------
-	// Exersise 1.3: Write a program to print Hello, World on the standard output
-	// --------
-	std::cout << "Hello, World" << std::endl;
+	// ---------------------------------------------------------------------------
+	// Exersise 1.4: 
+	// Our program used the addition operator, +, to add two numbers. Write a 
+	// program that uses the multiplication operator, *, to print the product 
+	// instead
+	// ---------------------------------------------------------------------------
+
+	std::cout << "Enter two numbers:" << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
 
 	return 0;
 }
