adaa5f7abc5437beb4dd78769c7cb12bfe59c6fd
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-16 21:30:14 +0200

    Excersise 1.3: Write a program to print Hello, World on the standard output.

--- primer/main.cpp
+++ primer/main.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-	std::cout << "Enter two numbers:" << std::endl;
-	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+	// --------
+	// Exersise 1.3: Write a program to print Hello, World on the standard output
+	// --------
+	std::cout << "Hello, World" << std::endl;
 
 	return 0;
 }
