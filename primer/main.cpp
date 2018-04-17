f1e69154624d044eb405850e488af8b926994c0c
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-17 07:31:14 +0200

    Excersise 1.10:  In addition to the ++ operator that adds 1 to its operand, there is a decrement operator (--) that subtracts 1. Use the decrement operator to write a while that prints the numbers from ten down to zero.

--- primer/main.cpp
+++ primer/main.cpp
@@ -4,17 +4,19 @@ int main()
 {
 
 	// ------------------------------------------------------------------------
-	// Excersise 1.9:
-	// Write a program that uses a while to sum the numbers from 50 to 100.
+	// Excersise 1.10:
+	// In addition to the ++ operator that adds 1 to its operand, there is a
+	// decrement operator (--) that subtracts 1. Use the decrement operator to
+	// write a while that prints the numbers from ten down to zero.
 	// ------------------------------------------------------------------------
 
-	int sum = 0, val = 51;
-	// keep executing the while as long as val is less than or equal to 10
-	while (val <= 100) {
+	int sum = 0, val = 10;
+	// keep executing the while as long as val is greater than or equal to 0
+	while (val >= 0) {
 		sum += val;		// assigns sum + val to sum
-		++val;			// add 1 to val
+		--val;			// subtracts 1 from val
 	}
-	std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
+	std::cout << "Sum of 0 to 10 inclusive is " << sum << std::endl;
 	
 	return 0;
 }
