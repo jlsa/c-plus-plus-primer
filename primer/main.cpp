73f9cb44cfbe0bcf1fabeac732a3b5d3513bf1ec
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-17 07:27:53 +0200

    Excersise 1.9: Write a program that uses a while to sum the numbers from 50 to 100.

--- primer/main.cpp
+++ primer/main.cpp
@@ -2,13 +2,20 @@
 
 int main()
 {
-	int sum = 0, val = 1;
+
+	// ------------------------------------------------------------------------
+	// Excersise 1.9:
+	// Write a program that uses a while to sum the numbers from 50 to 100.
+	// ------------------------------------------------------------------------
+
+	int sum = 0, val = 51;
 	// keep executing the while as long as val is less than or equal to 10
-	while (val <= 10) {
+	while (val <= 100) {
 		sum += val;		// assigns sum + val to sum
 		++val;			// add 1 to val
 	}
-	std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
-
+	std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
+	
 	return 0;
 }
+
